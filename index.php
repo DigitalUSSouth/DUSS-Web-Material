6b77a03052f2832e61bf5fe88bb227fc63a2ecca
Emily Shea <sheaem@email.sc.edu>
2016-02-22 12:06:32 -0500

    Update and rename newprojectgallery.html to index.php

---
+++ index.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title> Example Project Gallery </title>
+
+		<!--Slick Carousel-->
+		<link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+		<link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+		
+		<link href="style.css" rel="stylesheet" type="text/css" />
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+		<script>window.jQuery || document.write('>script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"><\/script>')</script>
+		<script src="fade.js" type="text/javascript"></script>
+		<script src="pullup.js" type="text/javascript"></script>
+
+	</head>
+	<body>
+		<div class="projectcarousel">
+			<div class="column">
+				<div id="wrapper">
+					<img class="hoverimg" src="http://lichen.csd.sc.edu/duss/img/thumb/simms.png" height="170px" alt="Simms Initiatives" />
+					<p class="text">Simms Initiatives</p>
+				</div>
+				<div id="wrapper">
+					<img class="hoverimg" src="http://lichen.csd.sc.edu/duss/img/thumb/southworth.png" height="170px" alt="E.D.E.N. Southworth Collection" />
+					<p class="text">E.D.E.N. Southworth Collection</p>
+			    </div>
+			</div>
+			<div class="column">
+				<div id="wrapper">
+					<img class="hoverimg" src="http://lichen.csd.sc.edu/duss/img/thumb/burning-of-columbia.png" height="170px" alt="
+					The Burning of Columbia" />
+					<p class="text">The Burning of Columbia</p>
+				</div>
+				<div id="wrapper">
+					<img class="hoverimg" src="http://lichen.csd.sc.edu/duss/img/thumb/american-heritage.png" height="170px" alt="American Heritage Vegetables" />
+					<p class="text">American Heritage Vegetables</p>
+				</div>
+			</div>
+			<div class="column">
+				<div id="wrapper">
+					<img class="hoverimg" src="http://lichen.csd.sc.edu/duss/img/thumb/civil-war.png" height="170px" alt="South Carolina and the Civil War" />
+					<p class="text">South Carolina and the Civil War</p>
+				</div>
+				<div id="wrapper">
+					<img class="hoverimg" src="http://lichen.csd.sc.edu/duss/img/thumb/pope-brown.png" height="170px" alt="Ethelind Pope Brown Collection of South Carolina National History" />
+					<p class="text">Ethelind Pope Brown Collection of South Carolina National History</p>
+				</div>
+			</div>
+			<div class="column">
+				<div id="wrapper">
+					<img class="hoverimg" src="http://lichen.csd.sc.edu/duss/img/thumb/george-lagrange-cook.png" height="170px" alt="George LaGrange Cook Photograph Collection, c.1880-1895" />
+					<p class="text">George LaGrange Cook Photograph Collection, c.1880-1895</p>
+				</div>
+				<div id="wrapper">
+					<img class="hoverimg" src="http://lichen.csd.sc.edu/duss/img/thumb/ravenel.png" height="170px" alt="Plants and Planters - Henry William Ravenel" />
+					<p class="text">Plants and Planters - Henry William Ravenel</p>
+				</div>
+			</div>
+			<div class="column">
+				<div id="wrapper">
+					<img class="hoverimg" src="http://lichen.csd.sc.edu/duss/img/thumb/sphr.png" height="170px" alt="Southern Periodical Humor Repository" />
+					<p class="text">Southern Periodical Humor Repository</p>
+				</div>
+			</div>
+		</div>
+        <!--Slick Carousel Scripts-->
+		<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+		<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+		<script type="text/javascript" src="slick/slick.min.js"></script>
+		<script type="text/javascript" src="carousel.js"></script>
+
+		<body bgcolor="#376FB7">
+	</div>
+	</body>
+</html>
